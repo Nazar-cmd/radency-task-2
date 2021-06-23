7394316e38cd5d92f7f7d2aaf8e93b31fd282b48
Nazar Velychko <nazar.vel@gmail.com>
2021-06-23 21:11:07 +0300

    added .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	extends: ["prettier"],
+	root: true,
+	parser: "babel-eslint",
+	plugins: ["import", "babel", "react", "react-hooks", "prettier"],
+	settings: {
+		react: {
+			version: "16.9"
+		},
+		"import/resolver": {
+			node: {
+				moduleDirectory: ["node_modules", "/"]
+			}
+		}
+	},
+	rules: {
+		//semi: [2, "never"],
+		// 'no-console': 'error',
+		"react/forbid-prop-types": 0,
+		"react/require-default-props": 0,
+		"react/jsx-filename-extension": 0,
+		"import/no-named-as-default": 0,
+		"no-return-await": 2,
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+		"prettier/prettier": [
+			"error",
+			{
+				singleQuote: false,
+				trailingComma: "none",
+				semi: true,
+				bracketSpacing: true,
+				jsxBracketSameLine: true,
+				printWidth: 80,
+				tabWidth: 2,
+				useTabs: true
+			}
+		]
+	}
+};
